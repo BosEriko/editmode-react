c6374646691dd8aeff943f591938c23535719e3c
BosEriko <bos.eriko@gmail.com>
2020-07-27 08:10:48 +0800

    use native file

--- src/utils/renderChunk.jsx
+++ src/utils/renderChunk.jsx
@@ -1,53 +1,36 @@
 import DOMpurify from "dompurify";
 import React from "react";
-import { Platform, Text, Image } from 'react-native';
 
 export const renderChunk = (cnk, props) => {
-  const sanitizedContent = Platform.OS === 'web'
-    ? DOMpurify.sanitize(cnk.content)
-    : cnk.content;
-
-  let chunk = { ...cnk, content: sanitizedContent };
+  let chunk = { ...cnk, content: DOMpurify.sanitize(cnk.content) };
   switch (chunk.chunk_type) {
     case "single_line_text":
-      return Platform.OS === 'web'
-        ? (<span
-            data-chunk={chunk.identifier}
-            data-chunk-editable={true}
-            data-chunk-content-key={chunk.content_key}
-            data-chunk-type="single_line_text"
-            key={chunk.identifier}
-            {...props}
-          >
-            {chunk.content}
-          </span>)
-        : (<Text
-            key={chunk.identifier}
-            {...props}
-          >
-            {chunk.content}
-          </Text>);
+      return (
+        <span
+          data-chunk={chunk.identifier}
+          data-chunk-editable={true}
+          data-chunk-content-key={chunk.content_key}
+          data-chunk-type="single_line_text"
+          key={chunk.identifier}
+          {...props}
+        >
+          {chunk.content}
+        </span>
+      );
     case "image":
-      return Platform.OS === 'web'
-        ? (<img
-            src={chunk.content}
-            data-chunk={chunk.identifier}
-            data-chunk-editable={false}
-            data-chunk-content-key={chunk.content_key}
-            data-chunk-type="image"
-            alt=""
-            key={chunk.identifier}
-            {...props}
-          />)
-        : (<Image
-              source={{
-                uri: 'https:' + chunk.content,
-                isStatic: true,
-              }}
-            />);
+      return (
+        <img
+          src={chunk.content}
+          data-chunk={chunk.identifier}
+          data-chunk-editable={false}
+          data-chunk-content-key={chunk.content_key}
+          data-chunk-type="image"
+          alt=""
+          key={chunk.identifier}
+          {...props}
+        />
+      );
     default:
-      return Platform.OS === 'web'
-        ? <span {...props}>{chunk.content}</span>
-        : <Text {...props}>{chunk.content}</Text>;
+      return <span {...props}>{chunk.content}</span>;
   }
 };
